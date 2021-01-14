77bdf4ef263b19cc594d7d134644fc38cc9746c6
Subeen <bueong2kavva2@gmail.com>
2021-01-14 20:50:35 +0900

    2021.01.14 TextRPG2 ver1

---
+++ TextRPG_Csharp/TextRPG2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TextRPG_Csharp.TextRPG2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Knight();
+            Monster monster = new Orc();
+
+            int damage = player.GetAttack();
+            monster.OnDamage(damage);
+        }
+    }
+}
